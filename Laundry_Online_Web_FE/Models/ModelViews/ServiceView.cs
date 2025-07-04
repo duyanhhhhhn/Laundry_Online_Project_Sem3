3e148b0226d2d29bf8a554f74ae7ecb53c8e576a
Duy Anh <150674221+duyanhhhhhn@users.noreply.github.com>
2025-07-04 23:01:59 +0700

    config folder

---
+++ Laundry_Online_Web_FE/Models/ModelViews/ServiceView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laundry_Online_Web_FE.Models.ModelViews
+{
+    public class ServiceView
+    {
+    }
+}
---
+++ Laundry_Online_Web_FE/Models/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laundry_Online_Web_FE.Models.Repositories
+{
+    internal interface IRepository<T> where T : class
+    {
+        void Create(T entity);
+        int Update(T entity);
+        int Delete(T entity);
+        HashSet<T> All();
+        HashSet<T> findAll();
+        HashSet<T> FindByKeyword(string keyword);
+    }
+}
