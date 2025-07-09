0496af341dcdc54fd61f75edd38aebfa41fce3cb
Pham Quoc Thang <quocthangk1p2@gmail.com>
2025-07-10 01:28:36 +0700

    Client Login+Register View

--- Laundry_Online_Web_FE/Controllers/HomeController.cs
+++ Laundry_Online_Web_FE/Controllers/HomeController.cs
@@ -26,5 +26,15 @@ namespace Laundry_Online_Web_FE.Controllers
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
     }
 }
