87ceb5bdc1dba3565d7a1121d7b4dab6af030602
ngoclu90 <145348468+ngoclu90@users.noreply.github.com>
2025-07-11 14:18:32 +0700

    InvoiceItem
    tạo Model, tạo Repo

--- Laundry_Online_Web_FE/Models/ModelViews/InvoiceItemView.cs
+++ Laundry_Online_Web_FE/Models/ModelViews/InvoiceItemView.cs
@@ -7,5 +7,15 @@ namespace Laundry_Online_Web_FE.Models.ModelViews
 {
     public class InvoiceItemView
     {
+        public int Id { get; set; } = 0;
+        public int InvoiceId { get; set; } = 0;
+        public string ItemName { get; set; } = "";
+        public decimal Quantity { get; set; } = 0;
+        public decimal UnitPrice { get; set; } = 0;
+        public decimal SubTotal { get; set; } = 0;
+        public string BarCode { get; set; } = "";
+        public int ItemStatus { get; set; } = 0;
+        public int ServiceId { get; set; } = 0;
+
     }
 }
